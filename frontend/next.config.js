26024493aa28acd817c221d2178c187c8eb4979a
Paul-Karonji <karonjipaul.w@gmail.com>
2025-11-25 13:38:28 +0300

    fix: enable experimental optimizeCss to force Vercel CSS generation

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,6 +13,9 @@ const nextConfig = {
       },
     ],
   },
+  experimental: {
+    optimizeCss: true,
+  },
   env: {
     NEXT_PUBLIC_APP_NAME: process.env.NEXT_PUBLIC_APP_NAME,
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
